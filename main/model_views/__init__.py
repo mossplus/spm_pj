f4a0562f14e7253d934ad8d21c664fa476a78818
swzhangslg <swzhangslg@qq.com>
2023-05-15 22:44:52 +0800

    dl

--- main/model_views/__init__.py
+++
@@ -1,3 +0,0 @@
-from .user_views import *
-from .room_views import *
-from .rsv_views import *
--- main/services/__init__.py
+++
@@ -1,3 +0,0 @@
-from .user_service import *
-from .room_services import *
-from .rsv_services import *
